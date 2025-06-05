ba1923f5a0517c3215954470a43b64018ec8f99b
ADITYA RAJ <139373669+adityaRaj369@users.noreply.github.com>
2025-06-05 10:49:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,16 +9,29 @@ const ACTIONS = require("./Actions");
 
 const app = express();
 const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://coding-collaborator-ai-compiler.vercel.app/" // Replace with your Vercel URL
+    ],
     methods: ["GET", "POST"],
   },
 });
-
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
-
+// app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://coding-collaborator-ai-compiler.vercel.app/" // Replace with your Vercel URL
+  ]
+}));
 const userSocketMap = {};
 const mongoURI = process.env.MONGO_URI;
 let db;
